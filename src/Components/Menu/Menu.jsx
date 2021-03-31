c338aa87063f096829d19e15c326016a68eafc0a
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-03-31 19:38:05 -0300

    Ajustes em nomes de classes e inclusão de logo

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -8,19 +8,24 @@ import { IconContext } from "react-icons";
 
 function Menu() {
   const [sidebar, setSidebar] = useState(false);
-
   const showSideBar = () => setSidebar(!sidebar);
 
   return (
     <>
       <IconContext.Provider value={{ color: "#fff" }}>
         <div className="menu">
-          <Link to="#" className="menu-botao">
-            <FaIcons.FaBars onClick={showSideBar} />
+          <Link to="#" className="menu-botao" onClick={showSideBar}>
+            <FaIcons.FaBars />
+            <span className="menu-texto">
+              Menu
+            </span>
+          </Link>
+          <Link to="/" className="menu-logo">
+            Muffin Educacional
           </Link>
         </div>
         <nav className={sidebar ? "menu-lista active" : "menu-lista"}>
-          <ul className="menu-lista__items" onClick={showSideBar}>
+          <ul className="menu-lista__items">
             <li className="menu-toggle">
               <Link to="#" className="menu-botao">
                 <AiIcons.AiOutlineClose onClick={showSideBar} />
@@ -29,7 +34,7 @@ function Menu() {
             {MenuData.map((item, index) => {
               return (
                 <li key={index} className={item.cName}>
-                  <Link to={item.path}>
+                  <Link to={item.path} onClick={showSideBar}>
                     {item.icon}
                     <span>{item.title}</span>
                   </Link>
